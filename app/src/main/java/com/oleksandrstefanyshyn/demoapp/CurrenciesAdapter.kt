b63e7f8bcadf5c9a50db0258921c41f49c035132
Oleksandr <oleksandr.stefanyshyn@gmail.com>
2019-10-01 23:14:17 +0300

    - Show currencies list - Add item click listener

---
+++ app/src/main/java/com/oleksandrstefanyshyn/demoapp/CurrenciesAdapter.kt
@@ -0,0 +1,40 @@
+package com.oleksandrstefanyshyn.demoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_currency.view.*
+
+class CurrenciesAdapter : RecyclerView.Adapter<CurrenciesAdapter.CurrenciesViewHolder>() {
+    private val clickSubject = PublishSubject.create<String>()
+    private var currencies: List<String> = emptyList()
+    val clickEvent: Observable<String> = clickSubject
+
+    fun update(list: List<String>) {
+        currencies = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_currency, parent, false)
+            .let(::CurrenciesViewHolder)
+
+    override fun getItemCount(): Int = currencies.size
+
+    override fun onBindViewHolder(holder: CurrenciesViewHolder, position: Int) {
+        holder.containerView.apply {
+            currency_name.text = currencies[position]
+            setOnClickListener {
+                clickSubject.onNext(currencies[holder.adapterPosition])
+            }
+        }
+    }
+
+    class CurrenciesViewHolder(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView), LayoutContainer
+}
--- app/src/main/java/com/oleksandrstefanyshyn/demoapp/CurrenciesListFragment.kt
+++ app/src/main/java/com/oleksandrstefanyshyn/demoapp/CurrenciesListFragment.kt
@@ -1,21 +1,23 @@
 package com.oleksandrstefanyshyn.demoapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-
-private const val TAG = "CurrenciesListFragment"
+import kotlinx.android.synthetic.main.fragment_currencies_list.*
 
 class CurrenciesListFragment : Fragment() {
     private val currenciesApi: CurrenciesApi by lazy {
         injector.currenciesApi
     }
+    private val disposable = CompositeDisposable()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,12 +27,30 @@ class CurrenciesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val currenciesAdapter = CurrenciesAdapter()
 
-        val disposable = currenciesApi.currenciesList()
+        currencies_list.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = currenciesAdapter
+            addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+        }
+
+        currenciesAdapter.clickEvent.subscribe {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+        }.also {
+            disposable.add(it)
+        }
+
+        currenciesApi.currenciesList()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { currencies -> Log.d(TAG, "Currencies: $currencies") },
+                { currencies -> currenciesAdapter.update(currencies.rates.keys.toList()) },
                 {
                     Toast.makeText(
                         requireContext(),
@@ -38,6 +58,13 @@ class CurrenciesListFragment : Fragment() {
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-            )
+            ).also {
+                disposable.add(it)
+            }
+    }
+
+    override fun onDestroyView() {
+        disposable.dispose()
+        super.onDestroyView()
     }
 }
