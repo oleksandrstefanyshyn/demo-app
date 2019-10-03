25eca475f6f06e64ce13b52f94830dcd17ac72e8
Oleksandr <oleksandr.stefanyshyn@gmail.com>
2019-10-03 22:39:20 +0300

    Add exange currencies screen

--- app/src/main/java/com/oleksandrstefanyshyn/demoapp/ExchangeFragment.kt
+++ app/src/main/java/com/oleksandrstefanyshyn/demoapp/ExchangeFragment.kt
@@ -1,5 +1,17 @@
 package com.oleksandrstefanyshyn.demoapp
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_exchange.*
+
 class ExchangeFragment : BaseFragment() {
     override val layoutId: Int = R.layout.fragment_exchange
+    val args: ExchangeFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        currency_name.text = args.currencyName
+    }
 }
